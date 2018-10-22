b3f80754643c3c6383b6e66897913d8c1d734ef7
hpysirius <hpysirius@gmail.com>
2018-10-22 16:23:15 +0800

    feat:联调文章列表，新增http工具类

---
+++ src/configs/server.js
@@ -0,0 +1,8 @@
+export default {
+  dev: {
+    admin: 'http://localhost:7002/'
+  },
+  prod: {
+    admin: 'http://localhost:7002/'
+  }
+}
---
+++ src/service/index.js
@@ -0,0 +1,5 @@
+import Http from '../utils/Http'
+
+const { post } = Http.create('admin')
+
+export const getArticle = params => post({ url: 'blog/article/list', params })
