b02c67579047bd48e43f249bfa1dc36c094e63fd
hpysirius <hpysirius@gmail.com>
2018-10-25 16:22:05 +0800

    feat:文章详情联调

--- src/service/index.js
+++ src/service/index.js
@@ -3,3 +3,4 @@ import Http from '../utils/Http'
 const { post } = Http.create('admin')
 
 export const getArticle = params => post({ url: 'blog/article/list', params })
+export const getDetail = params => post({ url: 'blog/article/detail', params })
